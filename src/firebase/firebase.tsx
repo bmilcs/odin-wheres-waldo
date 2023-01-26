1d73b3848da8832e50ac12954e97f5ae303866a5
Bryan Miller <bmilcs@yahoo.com>
2023-01-26 16:42:39 -0500

    Add firebase packages, confs & functions/

---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABzNK-oeIQdF0ZrqZGxca9avUGzMNUXmw",
+  authDomain: "odin-wheres-waldo-a12f6.firebaseapp.com",
+  projectId: "odin-wheres-waldo-a12f6",
+  storageBucket: "odin-wheres-waldo-a12f6.appspot.com",
+  messagingSenderId: "128540815863",
+  appId: "1:128540815863:web:b6ef8fb372fd0d5d7fbef2",
+};
+
+const app = initializeApp(firebaseConfig);
