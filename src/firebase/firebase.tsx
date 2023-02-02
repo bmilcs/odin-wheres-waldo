b7a57cce8b2bcf2d359af75c727b044b7200c4c9
Bryan Miller <bmilcs@yahoo.com>
2023-02-02 16:49:39 -0500

    Add saveToLeaderboard function

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -4,6 +4,7 @@ import {
   httpsCallable,
   // connectFunctionsEmulator,
 } from "firebase/functions";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABzNK-oeIQdF0ZrqZGxca9avUGzMNUXmw",
@@ -14,16 +15,44 @@ const firebaseConfig = {
   appId: "1:128540815863:web:b6ef8fb372fd0d5d7fbef2",
 };
 
-// LIVE VERSION:
-
 const app = initializeApp(firebaseConfig);
 const functions = getFunctions(app);
+const db = getFirestore(app);
 
 // LOCAL TEST VERSION:
 
 // connectFunctionsEmulator(functions, "localhost", 5001);
 
+// CLOUD FUNCTIONS
+
 export const validateCharacterPosition = httpsCallable(
   functions,
   "validateCharacterPosition"
 );
+
+export const saveToLeaderboard = httpsCallable(functions, "saveToLeaderboard");
+
+// LOCAL FUNCTIONS
+
+export interface LeaderboardEntry {
+  name: string;
+  time: number;
+}
+
+export const getLeaderboardData = async (levelID: string) => {
+  try {
+    const docRef = doc(db, "levels", levelID);
+    const snapshot = await getDoc(docRef);
+    const data = await snapshot.data();
+
+    if (!data || !data.scores) return;
+
+    const scores: LeaderboardEntry[] = data.scores;
+
+    return scores.sort((a, b) => {
+      return a.time > b.time ? 1 : -1;
+    });
+  } catch {
+    console.log("error");
+  }
+};
