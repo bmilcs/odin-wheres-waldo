b5f8d6622c8a8e563216a6498da1b9ea26fa43b3
Bryan Miller <bmilcs@yahoo.com>
2023-02-04 14:51:45 -0500

    Refactor time formatter

--- src/utils/formatTime.tsx
+++ src/utils/formatTime.tsx
@@ -1,15 +1,8 @@
 const formatTime = (time: number): string => {
-  let sec: string, min: string;
-
-  if (time < 60) {
-    sec = time.toString().padStart(2, "0");
-    min = "00";
-  } else {
-    min = Math.floor(time / 60)
-      .toString()
-      .padStart(2, "0");
-    sec = (time % 60).toString();
-  }
+  const min = Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0");
+  const sec = (time % 60).toString().padStart(2, "0");
 
   return `${min}:${sec}`;
 };
