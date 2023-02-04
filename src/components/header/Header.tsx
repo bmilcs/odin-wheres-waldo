7a8e2c408fd49bd0cb965ff9a54f15858938c260
Bryan Miller <bmilcs@yahoo.com>
2023-02-04 14:50:44 -0500

    Add github link to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,16 @@
 import "./Header.scss";
 import waldoIcon from "../../assets/characters/waldo-icon.png";
+import GitHub from "../github/Github";
 
 function Header() {
-  const PageTitle = (
-    <div className="title__container">
-      <img src={waldoIcon} alt="Waldo Headshot" className="title__img" />
-      <h1 className="title__text">Where's Waldo?</h1>
-    </div>
-  );
-
   return (
     <header>
       <div className="column">
-        {PageTitle}
-        <p>PLACEHOLDER</p>
+        <div className="title">
+          <img src={waldoIcon} alt="Waldo Headshot" className="title__img" />
+          <h1 className="title__text">Where's Waldo?</h1>
+        </div>
+        <GitHub />
       </div>
     </header>
   );
